42f84d805d1f2cb5f86db52cf1173b51554756ae
Reygaia <mrcobanfrog@gmail.com>
2024-04-03 15:47:41 +0700

    Commit 4-3-2024 mon BMTT

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print('trung dbrr')
-print('testing shit')
+
+
+class animal:
+    def __init__(self, species, type):
+        self.species = species
+        self.type = type
+
+
+class bear(animal):
+    def __init__(self, size, gender, species):
+        super().__init__(species=species, type="Mammal")
+        self.size = size
+        self.gender = gender
+
+
+Bear1 = bear(50, 'male','brown bear')
+
+print(Bear1.size)
+print(Bear1.gender)
+print(Bear1.species)
+print(Bear1.type)
